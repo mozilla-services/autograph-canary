96c986e01b24da90a182d5e03ee5c8149041dba7
Greg Guthe <gguthe@mozilla.com>
2021-04-20 14:40:17 -0400

    add test_canary.sh script

---
+++ bin/test_canary.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+# invoke a test canary run in a lambda monitor
+ERROR=$(curl -w '\n' -X POST 'http://localhost:8080/2015-03-31/functions/function/invocations' -d '{"env": {"signed_XPI": "https://searchfox.org/mozilla-central/source/toolkit/mozapps/extensions/test/xpcshell/data/signing_checks/signed1.xpi", "unsigned_XPI": "https://searchfox.org/mozilla-central/source/toolkit/mozapps/extensions/test/xpcshell/data/signing_checks/unsigned.xpi"}}')
+echo "got err: ${ERROR}"
+# test "$ERROR = null"
