fa9611d71dfe0c01fe0709a25333d1f252d3cf6a
Greg Guthe <gguthe@mozilla.com>
2021-06-03 16:56:26 -0400

    add version.json to image

---
+++ version.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+cd "$(dirname "$0")"
+# create a version.json per https://github.com/mozilla-services/Dockerflow/blob/main/docs/version_object.md
+printf '{"commit":"%s","version":"%s","source":"https://github.com/%s/%s","build":"%s"}\n' \
+"$CIRCLE_SHA1" \
+"$CIRCLE_TAG" \
+"$CIRCLE_PROJECT_USERNAME" \
+"$CIRCLE_PROJECT_REPONAME" \
+"$CIRCLE_BUILD_URL" > version.json
