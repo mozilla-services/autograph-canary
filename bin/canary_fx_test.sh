a3aa2d0135c735d18f230cdc5e0f8a53409f847b
Greg Guthe <gguthe@mozilla.com>
2021-06-10 14:30:23 -0400

    list mount in fx only debug script

--- bin/canary_fx_test.sh
+++ bin/canary_fx_test.sh
@@ -3,6 +3,8 @@
 set -ev
 set -o pipefail
 
+df -h
+
 /function/firefox/firefox/firefox -xpcshell -g /function/firefox/firefox/ -a /function/firefox/firefox/browser -f /function/tlscanary/js/worker_common.js /function/tests/content_signature_test.js <<EOF
 {"mode":"wakeup"}
 {"mode":"quit"}
